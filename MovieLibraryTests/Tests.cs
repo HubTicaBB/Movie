5ef74d1b7ba209be7f5f272f93c77da477dfdf40
HubTicaBB <tijana.ilic87@protonmail.com>
2021-01-22 02:12:14 +0100

    Remove shit

--- MovieLibraryTests/Tests.cs
+++ MovieLibraryTests/Tests.cs
@@ -9,50 +9,6 @@ namespace MovieLibraryTests
     [TestClass]
     public class Tests
     {
-        [TestMethod]
-        public void Toplist_ReturnsListOfStrings()
-        {
-            var controller = new MovieController();
-
-            var actual = controller.Toplist().ToList();
-
-            Assert.IsInstanceOfType( actual, typeof(List<string>));
-        }
-
-        [TestMethod]
-        public void Toplist_ReturnsCount100()
-        {
-            var controller = new MovieController();
-
-            var actual = controller.Toplist().ToList().Count;
-
-            Assert.AreEqual(100, actual);
-        }
-
-        [TestMethod]
-        public void Toplist_ReturnsDescending()
-        {
-            var controller = new MovieController();
-
-            var actual = controller.Toplist();
-            var movies = controller.Toplist().ToList();
-            var ordered = movies.Toplist(x => x.rated).ToList();
-            var expected = ordered.Select(x => x.title).ToList();
-
-            Assert.IsTrue(expected.SequenceEqual(actual));
-        }
-
-        [TestMethod]
-        public void Toplist_ReturnsAscending()
-        {
-            var controller = new MovieController(new HttpClient());
-
-            var actual = controller.Toplist(true);
-            var movies = controller.Toplist(new HttpClient()).ToList();
-            var ordered = movies.OrderBy(x => x.rated).ToList();
-            var expected = ordered.Select(x => x.title).ToList();
-
-            Assert.IsTrue(expected.SequenceEqual(actual));
-        }
+        
     }
 }
